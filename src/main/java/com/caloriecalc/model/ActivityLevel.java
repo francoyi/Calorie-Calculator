49ee28306652804a25c67680a1bd094e037a5a18
shourya <shourya.h.vardhan@gmail.com>
2025-11-13 18:23:19 -0500

    Finish backend of TDEE calculation, update with main branch.

--- src/main/java/com/caloriecalc/model/ActivityLevel.java
+++ src/main/java/com/caloriecalc/model/ActivityLevel.java
@@ -1,15 +1,16 @@
 package com.caloriecalc.model;
 
 //ballpark vals used here
-public class ActivityLevel {
-    double VERY_LIGHT = 1.2;
-    double LIGHT = 1.39;
-    double MEDIUM = 1.57;
-    double HIGH = 1.75;
-    double EXTREME = 1.9;
+public enum ActivityLevel {
+    VERY_LIGHT(1.2),
+    LIGHT(1.39),
+    MEDIUM(1.57),
+    HIGH(1.75),
+    EXTREME(1.9);
 
-    private final double multiplier;
+    public final double multiplier;
     ActivityLevel(double multiplier) {
+
         this.multiplier = multiplier;
     }
 }
--- src/main/java/com/caloriecalc/service/CalculateTDEEInteractor.java
+++ src/main/java/com/caloriecalc/service/CalculateTDEEInteractor.java
@@ -1,9 +1,8 @@
 package com.caloriecalc.service;
 
-import com.caloriecalc.port.tdee.BMRFormula;
-import com.caloriecalc.port.tdee.CalculateTDEEInputBoundary;
-import com.caloriecalc.port.tdee.CalculateTDEEInputData;
-import com.caloriecalc.port.tdee.CalculateTDEEOutputBoundary;
+import com.caloriecalc.model.ActivityLevel;
+import com.caloriecalc.model.UserMetrics;
+import com.caloriecalc.port.tdee.*;
 
 public class CalculateTDEEInteractor implements CalculateTDEEInputBoundary {
     private final BMRFormula bmrFormula;
@@ -16,6 +15,29 @@ public class CalculateTDEEInteractor implements CalculateTDEEInputBoundary {
 
     @Override
     public void execute(CalculateTDEEInputData input) {
+        try {
+            if (input.ageYears() < 18) {
+                presenter.presentValidationError("Age must be 18 or above.");
+            } else if (input.weight() <= 0) {
+                presenter.presentValidationError("Weight must be greater than 0.");
+            } else if (input.height() <= 0) {
+                presenter.presentValidationError("Height must be greater than 0.");
+            }
 
+
+            final double weightKg = input.metric() ? input.weight() : input.weight() * 0.453592;
+            final double heightCm = input.metric() ? input.height() : input.height() * 2.54;
+
+            UserMetrics userMetrics = new UserMetrics(input.ageYears(), weightKg, heightCm, input.sex());
+            double userBMR = bmrFormula.computeBmr(userMetrics);
+
+            ActivityLevel lvl = input.activityLevel();
+            double userTDEE = userBMR * lvl.multiplier;
+
+            presenter.present(new CalculateTDEEOutputData(userBMR, userTDEE, bmrFormula.name(), lvl.multiplier));
+
+        } catch (IllegalArgumentException ex) {
+            presenter.presentValidationError(ex.getMessage());
+        }
     }
 }
