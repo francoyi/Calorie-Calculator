412f0a12255dc9b5eb3109f38729cc97bdd215d9
shourya <shourya.h.vardhan@gmail.com>
2025-11-19 00:24:10 -0500

    Add test folder to src/

--- src/main/java/com/caloriecalc/repo/JsonUserMetricsRepository.java
+++ src/main/java/com/caloriecalc/repo/JsonUserMetricsRepository.java
@@ -3,12 +3,9 @@ package com.caloriecalc.repo;
 import com.caloriecalc.model.UserMetrics;
 import com.caloriecalc.port.UserMetricsRepository;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class JsonUserMetricsRepository implements UserMetricsRepository {
 
